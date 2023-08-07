3eb34135a4f7070ef77311588eb813910983f783
Andrew Hood <andrewhood125@gmail.com>
2023-08-06 18:47:47 -0700

    Add LogFacility

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@
 
 source "https://rubygems.org"
 
-# Specify your gem's dependencies in log_facility.gemspec
 gemspec
 
 gem "rake"
--- lib/log_facility.rb
+++ lib/log_facility.rb
@@ -3,6 +3,53 @@
 require_relative "log_facility/version"
 
 module LogFacility
-  class Error < StandardError; end
-  # Your code goes here...
+  LEVELS = [
+    :EMERG,
+    :ALERT,
+    :CRIT,
+    :ERR,
+    :WARNING,
+    :NOTICE,
+    :INFO,
+    :DEBUG
+  ]
+
+  SUPPRESS_MAX=20
+
+  class Logger
+    def initialize(path, mode = :append, levels = LEVELS)
+      @logdev = File.open(path, 'w+')
+      @logdev.sync = true
+
+      case mode
+      when :truncate
+        @logdev.truncate(0)
+      end
+
+      # Define convenience methods for log levels
+      levels.each do |level|
+        define_singleton_method(level.downcase) do |*args|
+          write(level, args)
+        end
+      end
+
+      # Used to supress duplicate lines
+      @last_line = ""
+      @duplicate_count = 0
+    end
+
+    def write(*objects)
+      msg = objects.join(' ') + "\n"
+      return @duplicate_count += 1 if msg == @last_line && @duplicate_count < SUPPRESS_MAX
+
+      if @duplicate_count > 0
+        @logdev.write("#{Time.now.floor(3)} #{@last_line.chomp} suppressed #{@duplicate_count} times.\n")
+      end
+
+      @last_line = msg
+      @duplicate_count = 0
+
+      @logdev.write("#{Time.now.floor(3)} #{msg}")
+    end
+  end
 end
